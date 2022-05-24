0765f0a4fd6ad7391160cd5dc9ca3d9e0a13ab80
AlexKo-Salty <alex.178.ko@gmail.com>
2022-05-24 23:03:55 +0800

    Create schema for project

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+//Config Mongoose
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+
+//Set up User Schema
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true}
+})
+
+let User = mongoose.model('User',userSchema);
+
+//Set up Exercise Schema
+const exerciseSchema = new Schema({
+  userid: { type: String, required: true},
+  description: { type:String, required: true},
+  duration: { type:Number, required: true},
+  date: { type: Date, required: true}
+})
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
